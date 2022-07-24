8cddc90cb0de451f3eb459e85ef2dbe4d034add9
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-24 20:53:30 +0200

    Test creating a user

--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,14 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-import { Department, User } from '../modal/types';
+import { CreateUser, Department, User } from '../modal/types';
 
 import { UsersService } from './users.service';
 
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { combineLatest, map, take } from 'rxjs';
+
 describe('UsersService', () => {
   let service: UsersService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(UsersService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
@@ -16,28 +30,51 @@ describe('UsersService', () => {
   });
 
   describe('Store information', () => {
+    afterEach(() => httpTestingController.verify());
     it('The initial value should be an empty list', () => {
       service.users$.subscribe((list: User[]) => expect(list).toEqual([]));
     });
     it('If a user is inserted into the list it should exist', () => {
       const spy = spyOn(service, 'addUser').and.callThrough();
-      const fakeUser: User = {
-        id: '1',
+      const fakeUser: CreateUser = {
         name: 'Nick Jonas',
         email: 'jonas@gmail.com',
         department: Department.Development,
       };
-      service.addUser(fakeUser);
-      service.users$.subscribe((list: User[]) => {
-        expect(list).toHaveSize(1);
-        expect(list).toContain(fakeUser);
+
+      service
+        .addUser(fakeUser)
+        .subscribe((newUser) =>
+          expect(newUser).toEqual({ id: 1, ...fakeUser })
+        );
+
+      // AddUser should have made one request to POST
+      const req = httpTestingController.expectOne(
+        'http://localhost:3000/users'
+      );
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body).toEqual(fakeUser);
+
+      // Expect server to return the User after POST
+      const expectedResponse = new HttpResponse({
+        status: 201,
+        statusText: 'Created',
+        body: { id: 1, ...fakeUser },
       });
+      req.event(expectedResponse);
+
+      // Expect service to save new User
+      service.users$.subscribe((data) => {
+        expect(data).toContain({ id: 1, ...fakeUser });
+        expect(data).toHaveSize(1);
+      });
+
       expect(spy).toHaveBeenCalledWith(fakeUser);
     });
 
-    it('If a user is edited, it should exist the new version only', () => {
+    xit('If a user is edited, it should exist the new version only', () => {
       const fakeUser: User = {
-        id: '2',
+        id: 2,
         name: 'James Brown',
         email: 'brown@gmail.com',
         department: Department.Development,
@@ -52,15 +89,15 @@ describe('UsersService', () => {
       });
     });
 
-    it('If a user is deleted, it should not exist', () => {
+    xit('If a user is deleted, it should not exist', () => {
       const fakeUser: User = {
-        id: '1',
+        id: 1,
         name: 'James Brown',
         email: 'brown@gmail.com',
         department: Department.Development,
       };
       service.addUser(fakeUser);
-      service.deleteUser('1');
+      service.deleteUser(1);
       service.users$.subscribe((list: User[]) => {
         expect(list).not.toContain(fakeUser);
         expect(list).toHaveSize(0);
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { User } from '../modal/types';
+import { environment } from 'src/environments/environment';
+import { CreateUser, User } from '../modal/types';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +11,13 @@ import { User } from '../modal/types';
 export class UsersService {
   private readonly _users = new BehaviorSubject<User[]>([]);
   readonly users$ = this._users.asObservable();
-  constructor() {}
+  constructor(private readonly httpClient: HttpClient) {}
 
-  addUser(user: User) {
+  addUser(user: CreateUser) {
     const previousUsers = this._users.value;
-    this._users.next([...previousUsers, user]);
+    return this.httpClient
+      .post<User>(environment.url, user)
+      .pipe(tap((user) => this._users.next([...previousUsers, user])));
   }
 
   editUser(user: User) {
@@ -22,7 +27,7 @@ export class UsersService {
     this._users.next([...previousUsersFiltered, user]);
   }
 
-  deleteUser(id: string) {
+  deleteUser(id: number) {
     const previousUsersFiltered = this._users.value.filter((u) => u.id !== id);
     this._users.next([...previousUsersFiltered]);
   }

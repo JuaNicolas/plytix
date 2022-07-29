fc5ea5c9c7c3a92710ac7d9396fa9182c9df38ff
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-29 21:54:19 +0200

    App Component -> Hanlde logic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,93 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of, take } from 'rxjs';
 import { AppComponent } from './app.component';
+import { Department, User } from './models/types';
+import { UsersService } from './services/users.service';
+import { Button } from './ui/toggle-button/toggle-button.component';
+
+const devUserMock: User = {
+  id: 0,
+  name: 'Tino',
+  email: 'giordano@gmail.com',
+  department: Department.Development,
+  created: new Date().toISOString(),
+};
+const markUserMock: User = {
+  id: 0,
+  name: 'Marciano',
+  email: 'lucciano@gmail.com',
+  department: Department.Marketing,
+  created: new Date().toISOString(),
+};
+const mockUsers: User[] = [devUserMock, markUserMock];
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fakeUsersService: jasmine.SpyObj<UsersService>;
+
   beforeEach(async () => {
+    fakeUsersService = jasmine.createSpyObj<UsersService>(
+      'UsersService',
+      {
+        addUser: of(),
+      },
+      {
+        users$: of(mockUsers),
+      }
+    );
+
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, MatDialogModule, NoopAnimationsModule],
       declarations: [AppComponent],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+
+    component = TestBed.createComponent(AppComponent).componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'plytix'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('plytix');
+  describe('handleDisplay', () => {
+    beforeEach(() => spyOn(component, 'handleDisplay').and.callThrough());
+    afterEach(() => expect(component.handleDisplay).toHaveBeenCalled());
+
+    it('should handle user press Left Button', () => {
+      component.handleDisplay(Button.Left);
+      expect(component.display).toBeTrue();
+    });
+
+    it('should handle user press Right Button', () => {
+      component.handleDisplay(Button.Right);
+      expect(component.display).toBeFalse();
+    });
+  });
+
+  describe('Filter users by department', () => {
+    it('Should filter by Marketing', () => {
+      component.marketingUsers$.pipe(take(1)).subscribe((users) => {
+        expect(users).not.toContain(devUserMock);
+        expect(users).toContain(markUserMock);
+        expect(users).toHaveSize(1);
+      });
+    });
+    it('Should filter by Development', () => {
+      component.developmentUsers$.pipe(take(1)).subscribe((users) => {
+        expect(users).not.toContain(markUserMock);
+        expect(users).toContain(devUserMock);
+        expect(users).toHaveSize(1);
+      });
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { filter, map, mergeMap } from 'rxjs';
+import { CreateUser, Department } from './models/types';
+import { UsersService } from './services/users.service';
+import { ModalComponent } from './ui/modal/modal.component';
+import { Button } from './ui/toggle-button/toggle-button.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'plytix';
+  display = !Button.Left;
+  departments = Department;
+  marketingUsers$ = this.usersService.users$.pipe(
+    map((users) => users.filter((u) => u.department === Department.Marketing))
+  );
+  developmentUsers$ = this.usersService.users$.pipe(
+    map((users) => users.filter((u) => u.department === Department.Development))
+  );
+
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly modal: MatDialog
+  ) {}
+
+  handleDisplay(event: Button): void {
+    this.display = !event;
+  }
+
+  addUser(): void {
+    this.modal
+      .open<ModalComponent, null, CreateUser>(ModalComponent, {
+        panelClass: 'custom-dialog-container',
+        minWidth: 320,
+        width: '700px',
+      })
+      .afterClosed()
+      .pipe(
+        filter(Boolean),
+        mergeMap((user) => this.usersService.addUser(user))
+      )
+      .subscribe();
+  }
 }

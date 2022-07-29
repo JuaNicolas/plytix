3a73a0589a33726a64b6124d48bfde88df6b4fa9
Nicolas Juanico <nicolas.juany@gmail.com>
2022-07-29 22:47:39 +0200

    App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PillPipe } from './pill.pipe';
 import { CardComponent } from './ui/card/card.component';
+import { ListComponent } from './ui/list/list.component';
+import { ModalComponent } from './ui/modal/modal.component';
+import { TableComponent } from './ui/table/table.component';
 import { ToggleButtonComponent } from './ui/toggle-button/toggle-button.component';
 
 @NgModule({
-  declarations: [AppComponent, ToggleButtonComponent, CardComponent],
+  declarations: [
+    AppComponent,
+    ToggleButtonComponent,
+    CardComponent,
+    ModalComponent,
+    TableComponent,
+    ListComponent,
+    PillPipe,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    CommonModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatCardModule,
     MatChipsModule,
+    MatInputModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTableModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
